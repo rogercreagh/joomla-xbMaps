8f2858029f4c96a3792608daa7e56947c1fd00d6
rogercreagh <44980188+rogercreagh@users.noreply.github.com>
2021-09-10 22:23:35 +0100

    Revert "Delete local directory"
    This reverts commit a5400f49f7c73a2dd334270510ffab12024068ad.

---
+++ local/test.js
@@ -0,0 +1,26 @@
+	<link href="/administrator/templates/isis/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+	<link href="/media/com_xbosm/css/xbosm.css" rel="stylesheet" />
+	<link href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" rel="stylesheet" />
+	<link href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" rel="stylesheet" />
+	<link href="https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css" rel="stylesheet" />
+	<link href="http://joomla.localhost//media/com_xbosm/css/leaflet/leaflet.awesome-markers.css" rel="stylesheet" />
+	<link href="/media/regularlabs/css/style.min.css?v=21.6.21279" rel="stylesheet" />
+	<link href="/administrator/templates/isis/css/template.css?a9094c5ed0bba94ffa422dab99abd0f4" rel="stylesheet" />
+	<script type="application/json" class="joomla-script-options new">{"csrf.token":"6982fa72277adab4151d5926be110805","system.paths":{"root":"","base":"\/administrator"}}</script>
+	<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+	<script src="https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js"></script>
+	<script src="/media/com_xbosm/js/leaflet/leaflet.easyprint/bundle.js"></script>
+	<script src="http://joomla.localhost//media/com_xbosm/js/leaflet/leaflet.awesome-markers.min.js"></script>
+	<script src="http://joomla.localhost//media/com_xbosm/js/leaflet/leaflet-gpx.min.js"></script>
+	<script src="/media/com_xbosm/js/xbosm.js"></script>
+	<script src="/media/jui/js/jquery.min.js?a9094c5ed0bba94ffa422dab99abd0f4"></script>
+	<script src="/media/jui/js/jquery-noconflict.js?a9094c5ed0bba94ffa422dab99abd0f4"></script>
+	<script src="/media/jui/js/jquery-migrate.min.js?a9094c5ed0bba94ffa422dab99abd0f4"></script>
+	<script src="/media/system/js/core.js?a9094c5ed0bba94ffa422dab99abd0f4"></script>
+	<script src="/media/jui/js/cms.js?a9094c5ed0bba94ffa422dab99abd0f4"></script>
+	<script src="/media/regularlabs/js/script.min.js?v=21.6.21279"></script>
+	<script src="/media/jui/js/bootstrap.min.js?a9094c5ed0bba94ffa422dab99abd0f4"></script>
+	<!--[if lte IE 9]><script src="/media/system/js/polyfill.filter.js?a9094c5ed0bba94ffa422dab99abd0f4"></script><![endif]-->
+	<script src="/administrator/templates/isis/js/template.js?a9094c5ed0bba94ffa422dab99abd0f4"></script>
+	<!--[if lt IE 9]><script src="/media/jui/js/html5.js?a9094c5ed0bba94ffa422dab99abd0f4"></script><![endif]-->
+	<script>
